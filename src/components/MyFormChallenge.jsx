77949ed5813e9f7fd8b52f42ae4168159338d4a0
Verónica I Pérez S <v.isabel1989@gmail.com>
2024-11-24 23:12:36 +0100

    create

--- src/components/MyFormChallenge.jsx
+++ src/components/MyFormChallenge.jsx
@@ -3,11 +3,30 @@ import BoxColor from "./BoxColor";
 
 const colors = ["red", "gren", "pink", "yellow", "purple", "white", "blue", "aqua", "olive"]
 
-function MyForm() {
+function MyFormChallenge() {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value.toLowerCase()); // Aseguramos que sea minúscula
+  };
 
   return (
-    <></>
+    <>
+      <div>
+        <input
+          type="text"
+          placeholder="Escribe un color"
+          value={inputValue}
+          onChange={handleInputChange}
+        />
+        <div className="color-grid">
+        {colors.map((color) => (
+          <BoxColor key={color} color={color} inputValue={inputValue} />
+        ))}
+        </div>
+      </div>
+    </>
   );
 }
 
-export default MyForm;
+export default MyFormChallenge;
