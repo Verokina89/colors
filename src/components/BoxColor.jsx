172635eb632c2f1e28b2670e725c073d99e39e35
Verónica I Pérez S <v.isabel1989@gmail.com>
2024-11-24 23:16:02 +0100

    creacion deeeeeeeee  este verifica la coincidencia del valor del input de cada cuadro de color que cambiara con dinamismo estilo y texto

--- src/components/BoxColor.jsx
+++ src/components/BoxColor.jsx
@@ -1,7 +1,16 @@
-function boxColor ({color, value}) {
+import React from "react";
+
+function BoxColor({ color, inputValue }) {
+  const isMatch = color === inputValue;
+
   return (
-    <>
-    </> 
-  )
+    <div
+      className={`box ${color}`}
+      style={{ backgroundColor: isMatch ? color : "transparent" }}
+    >
+      {isMatch ? `¡Soy el color ${color}!` : "No soy el color"}
+    </div>
+  );
 }
-export default boxColor;  
+
+export default BoxColor;
