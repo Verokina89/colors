a45a1f35400a43cec2046f46f3da32d47be19f41
Verónica I Pérez S <v.isabel1989@gmail.com>
2024-11-26 20:09:03 +0100

    se agrega el atributo id='colorInput' para que el navegador pueda asociar el input con label con respecto a la accesibilidad.Tambien añadimos el atributo name='colorInput' que ayuda al navegador a idenfitiicar el campo durante el completado, ya que es util para cuando se manejan formularios con multioples entradas.La etiqueta con htmlForm='colorInput' para vincular el texto de label al input corespondiente. Solo faltarian ccs

--- src/components/MyFormChallenge.jsx
+++ src/components/MyFormChallenge.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 import BoxColor from "./BoxColor";
 
-const colors = ["red", "gren", "pink", "yellow", "purple", "white", "blue", "aqua", "olive"]
+const colors = ["red", "green", "pink", "yellow", "purple", "white", "blue", "aqua", "olive"]
 
 function MyFormChallenge() {
   const [inputValue, setInputValue] = useState("");
@@ -13,7 +13,10 @@ function MyFormChallenge() {
   return (
     <>
       <div>
+      <label htmlFor="colorInput">Escribe un color:</label>
         <input
+          id="colorInput" // Atributo id para accesibilidad
+          name="colorInput" // Atributo name para autofill
           type="text"
           placeholder="Escribe un color"
           value={inputValue}
